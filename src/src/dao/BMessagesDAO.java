7524014ed9313b738219b0703617aeb66fcad8c2
YSL2022ishigai <108640021+YSL2022ishigai@users.noreply.github.com>
2022-07-20 17:25:23 +0900

    石貝担当

---
+++ src/src/dao/BMessagesDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class BMessagesDAO
+ */
+@WebServlet("/BMessagesDAO")
+public class BMessagesDAO extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
