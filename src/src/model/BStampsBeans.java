10fe8dec58ac346b64ca7114761dd136c228469a
YSL2022-Moriya <108639938+YSL2022-Moriya@users.noreply.github.com>
2022-07-20 17:48:52 +0900

    【back】BProfileBeans作成

--- src/src/model/BStampsBeans.java
+++ src/src/model/BStampsBeans.java
@@ -2,7 +2,7 @@ package model;
 
 import java.io.Serializable;
 
-public class BStampsBeans implements  Serializable {
+public class BStampsBeans implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private int empId;
