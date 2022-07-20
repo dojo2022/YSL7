80384694e7eb02c802ba1a8a5c50ddad304330a4
YSL2022-saito <akane.saito@ysl.co.jp>
2022-07-20 18:32:19 +0900

    齊藤

--- src/src/servlet/BControllerServlet.java
+++ src/src/servlet/BControllerServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,20 +15,51 @@ import javax.servlet.http.HttpServletResponse;
 public class BControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		//★ページIDの取得
+				String pageId = request.getParameter("page_id");
+				//★ボタンの名前の取得
+				String button = request.getParameter("bt_name");
+				//次に飛ぶところのURLをNULLで宣言して、処理によって値を格納する
+				String url = null;
+
+
+		//page_id.equals("FH01") && button.equals("hr")  //フロントから管理画面を押された時
+
+		//page_id.equals("BL01") && button.equals("to_profile")//一覧から個人ページを表示するとき
+
+		//page_id.equals("BP01") && button.equals("back_list")個人ページから一覧にもどる
+
+		//page_id.equals("BN01") && button.equals("to_regist_note")メモの新規作成
+
+		//page_id.equals("BN01") && button.equals("back_memo")メモ新規画面から個人ページに戻る
+
+		//page_id.equals("BL01") && button.equals("to_regist_user") 新規社員登録画面に移動する
+
+		//page_id.equals("BR01") && button.equals("back")新規社員登録画面から一覧に戻る
+
+		//個人情報編集画面から個人ページに戻る
+
+		//page_id.equals("BL01") && button.equals("to_front")社員用画面に戻る
+
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		//新規社員登録の登録ボタン
+
+		//退職ボタン
+
+		//個人情報変更ボタン
+
+		//メモの保存ボタン
+
+		//メモの削除ボタン
+
+
+
 	}
 
 }
