3a9ca2543df969bf72bcf0c3d6fb3091de61ca30
ysl2022-kondo <yusuke.kondo@ysl.co.jp>
2022-07-20 18:35:18 +0900

    FrontControllerServlet.javaを更新

--- src/src/servlet/FrontControllerServlet.java
+++ src/src/servlet/FrontControllerServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +21,22 @@ public class FrontControllerServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String path=null;
+		if(request.getParameter("pageId")!=null && request.getParameter("submit")!=null) {
+			//ページIDの取得
+			String pageId =request.getParameter("page_id");
+			//ボタンの名前の取得
+			String button = request.getParameter("bt_name");
+			if()
+		}else {
+			//単にlogin画面に遷移させるだけのメソッド
+			path="/WEB-INF/jsp/login.jsp";
+		}
+
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+		dispatcher.forward(request, response);
+
 	}
 
 	/**
