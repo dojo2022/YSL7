455686e54f3d8df4ba5d08dfa25bab99ef1b7fd5
YSL2022-motohashi <misuzu.motohashi@ysl.co.jp>
2022-07-21 14:04:29 +0900

    LoginAction, LoginService, UsersDaoを更新

--- src/src/action/LoginAction.java
+++ src/src/action/LoginAction.java
@@ -1,5 +1,45 @@
 package action;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import model.Users;
+import service.LoginService;
+
 public class LoginAction {
+	HttpServletRequest request=null;
+
+	public LoginAction(HttpServletRequest request) {
+		this.request=request;
+	}
+
+	public String login() throws UnsupportedEncodingException  {
+		request.setCharacterEncoding("UTF-8");
+
+		//1.numberとpasswordをgetparameterで取得
+		String number = request.getParameter("number");
+		String password = request.getParameter("password");
+
+		//2．LoginServiceをnewする。
+		LoginService service = new LoginService();
+
+		//3．LoginServiceの中のloginメソッドを引数numberとpasswordを渡して呼び出す。
+		Users user = service.login(number, password);
+
+		//4．戻り値がnullじゃなかったら、sessionスコープに戻り値の値を格納する。
+		if (user == null) {
+			//5. home.jspにフォワードするpassをサーブレットに返す。
+			return "WEB-INF/jsp/login.jsp";
+		}else {
+			HttpSession session = request.getSession();
+			session.setAttribute("Users", user);
+			//5. home.jspにフォワードするpassをサーブレットに返す。
+			return "/WEB-INF/jsp/home.jsp";
+		}
+
+
+	}
 
 }
--- src/src/dao/UsersDao.java
+++ src/src/dao/UsersDao.java
@@ -1,5 +1,49 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.Users;
+
 public class UsersDao {
+	Connection con=null;
+
+	public UsersDao (Connection con) {
+		this.con = con;
+	}
+
+	//
+	public Users login(String number, String password) throws ClassNotFoundException, SQLException {
+
+		Users user = new Users();
+
+		// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// SQL文を準備する
+				String sql = "SELECT u_id, name, number FROM Users WHERE number = '?' AND password = '?'";
+				PreparedStatement pStmt = con.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setString(1,number);
+				pStmt.setString(2,password);
+
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果表をコレクションにコピーする
+				 rs.next();
+				 user.setUserId(Integer.parseInt(rs.getString("u_id")));
+			     user.setNumber(rs.getString("number"));
+			     user.setName(rs.getString("name"));
+
+
+
+
+				// 結果を返す
+				return user;
+	}
 
 }
