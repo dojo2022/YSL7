349ccb76e2f29145a9c22c77a9ec7a0c0709c737
sakumahitomi <hitomi.sakuma@ysl.co.jp>
2022-07-21 16:37:45 +0900

    社員退職処理のDAO

--- src/src/dao/BUserDAO.java
+++ src/src/dao/BUserDAO.java
@@ -14,6 +14,29 @@ public class BUserDAO {
 	public BUserDAO(Connection con) {
 		this.con=con;
 	}
+	//deleteUser
+	public int deleteUser(int empId) throws ClassNotFoundException, SQLException{
+
+		//あらかじめreturn用の変数を用意しておく
+		int ans=0;
+
+		// JDBCドライバを読み込む
+		Class.forName("org.h2.Driver");
+
+		// SQL文を準備する
+		String sql = "UPDATE Users SET flag=0 WHERE u_id=?";
+		PreparedStatement pStmt = con.prepareStatement(sql);
+
+		// SQL文を完成させる
+		pStmt.setInt(1,empId);
+
+		// SQL文を実行し、結果表を取得する
+		ans = pStmt.executeUpdate();
+
+		// 結果を返す
+		return ans;
+	}
+	//selectAllList
 	public ArrayList<BProfileAllBeans> selectAllList() throws SQLException, ClassNotFoundException {
 
 		//あらかじめreturn用の変数を用意しておく
@@ -56,4 +79,6 @@ public class BUserDAO {
 		return listAll;
 
 	}
+
+
 }
