9ae98ed40f04319462ebd5c0422959055c848d5e
sato <mizuho.sato@ysl.co.jp>
2022-07-21 15:33:22 +0900

    MessagesDao、MessageAction更新します

--- src/src/dao/MessagesDao.java
+++ src/src/dao/MessagesDao.java
@@ -18,12 +18,12 @@ public class MessagesDao {
 		Class.forName("org.h2.Driver");
 
 		// SQL文を準備する
-		String sql = "INSERT INTO Messages (from_u_id, message) VALUES (?, '?')";
+		String sql = "INSERT INTO Messages (from_u_id, message) VALUES (?, ?)";
 		PreparedStatement pStmt = con.prepareStatement(sql);
 
 		// SQL文を完成させる
-		pStmt.setString(1,"userId");
-		pStmt.setString(2,"message");
+		pStmt.setInt(1, userId);
+		pStmt.setString(2, message);
 
 		// SQL文を実行し、更新件数を取得する
 		int num = pStmt.executeUpdate();
