148ceb60e5b90a75c823ff9eabbc0c7f843887b7
YSL2022-saito <akane.saito@ysl.co.jp>
2022-07-22 10:36:57 +0900

    BUserService BUserDAO 変更

--- src/src/service/BUserService.java
+++ src/src/service/BUserService.java
@@ -28,23 +28,40 @@ public class BUserService {
 			BUserDAO buDao=new BUserDAO(con);
 			BUPUSDAO bupusDao = new BUPUSDAO(con);
 
+			//----Userに関するinsert-------------------
 			//daoのdeleteUserメソッドを呼び出す（引数に上記でもらったempIdを渡す）
-			int ans = buDao.registUser(String number, String name,String year, int gender);
+			int intgender = Integer.parseInt(gender);
+			int ans = buDao.registUser(String number, String name,String year, int intgender);
 
+			 //ansが0だった場合、insertできていない
 			if(ans != 0) {
 				empId = ans;
 				num1 = 1;
 			}
 
+			//----Postのinsert-------------------
+			int intPost1 = Integer.parseInt(post1);
+			int intPost2 = Integer.parseInt(post2);
+			int intPost3 = Integer.parseInt(post3);
+
 			//post3が空白じゃなかったら、registPost3
 			//post2が空白じゃなかったら、registPost2
 			//上記の二つ以外なら、registPost1
 			if(post3.length != 0 ) {
-				num2 = bupusDao.insertPost3();
+				num2 = bupusDao.insertPost3(intPost1, intPost2, intPost3);
 			} else if(post2.length != 0 ) {
-				num2 = bupusDao.insertPost2();
+				num2 = bupusDao.insertPost2(intPost1, intPost2);
 			} else {
-				num2 = bupusDao.insertPost1();
+				num2 = bupusDao.insertPost1(intPost1);
+			}
+
+			//----Sectionのinsert-------------------
+			int intSecId = Integer.parseInt(secId);
+			num3 = bupusDao.insertSection(empId, intSecId);
+
+			int sum = num1 + num2 + num3;
+			if(sum != 3) {
+				throw new SQLException();
 			}
 
 
