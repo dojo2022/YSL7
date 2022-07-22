d613bcdf46de32acaca25d5525daf6645287ea56
ysl2022-simadu <minami.shimazu@ysl.co.jp>
2022-07-22 15:22:27 +0900

    SearchEvaluationActionの更新

--- src/src/action/SearchEvaluationAction.java
+++ src/src/action/SearchEvaluationAction.java
@@ -12,33 +12,30 @@ import service.SearchEvaluationService;
 public class SearchEvaluationAction {
 	HttpServletRequest request = null;
 
-
 	//コンストラクタ
-	public SearchEvaluationAction(HttpServletRequest request){ //コンストラクタ
+	public SearchEvaluationAction(HttpServletRequest request) { //コンストラクタ
 		this.request = request;
 	}
 
 	//表示するメソッド
-	public String showEmp()throws UnsupportedEncodingException {
+	public String showEmp() throws UnsupportedEncodingException {
 		request.setCharacterEncoding("UTF-8");//日本語だよ
-
 
 		//何も入力されていなかったら全件検索にするので入力チェックは行わず、
 		//serviceをインスタンス化するよ。
-		SearchEvaluationService service= new SearchEvaluationService();
+		SearchEvaluationService service = new SearchEvaluationService();
 
 		//ServiceのSearchメソッドを実行するよ。
-		//service作ってないから現状だとエラー出るよ。
 		ArrayList<Users> userList = service.showEmp();
 
 		//userListの中身がnullだったらsearch_evaluation.jspに戻るよ。
-		if(userList.size() ==0) {
+		if (userList.size() == 0) {
 			request.setAttribute("errMsg", "検索したデータはありません");
 			return "/WEB-INF/jsp/search_evaluation.jsp";
-		//ちゃんと入っていたらservletにsearch_evaluation.jspのパスを送るよ。
-		}else{
+			//ちゃんと入っていたらservletにsearch_evaluation.jspのパスを送るよ。
+		} else {
 			HttpSession session = request.getSession();
-			request.setAttribute("list", userList);
+			session.setAttribute("list", userList);
 			return "/WEB-INF/jsp/search_evaluation.jsp";
 		}
 
