088e0aa9f9f6fe59633f6d455682863c12e86a92
sakumahitomi <hitomi.sakuma@ysl.co.jp>
2022-07-27 16:29:57 +0900

    Update uUpdate.js

--- src/WebContent/backjs/uUpdate.js
+++ src/WebContent/backjs/uUpdate.js
@@ -2,4 +2,10 @@
 document.getElementById("uUpdateReturn").addEventListener('click',()=>{
 	history.back();
 });
-
+function check(){
+	if(window.confirm("本当に退職しますか？")){
+		if(window.confirm("もうちょっと考えてみませんか？")){
+			window.alert("わかりました。新天地でのご活躍をお祈りします");
+		} else{history.back()};
+	} else{history.back()};
+};
